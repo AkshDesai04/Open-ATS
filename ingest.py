724d8465d4627592fe1a7df8c5537eaf49c5f738
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-23 14:07:48 +0530

    Added description ingest

--- ingest.py
+++ ingest.py
@@ -13,5 +13,46 @@ def ingest_profile(resume_path):
 
     return text
 
+def ingest_job_description(job_description_path):
+    position = ""
+    expected_experience = 0
+    skills = []
+    tools = []
+    # value = [] TODO
+
+    try:
+        with open(job_description_path, 'r') as file:
+            for line in file:
+                key, val = line.strip().split(':')
+                if key == 'position':
+                    position = val.strip()
+                elif key == 'expected experience':
+                    expected_experience = int(val.strip().split()[0])
+                elif key == 'skills':
+                    skills = [skill.strip() for skill in val.split(',')]
+                elif key == 'tools':
+                    tools = [tool.strip() for tool in val.split(',')]
+                # elif key == 'value':
+                #     value = [{'experience': int(val.strip().split('=')[1])}, {'skill': int(val.strip().split('=')[2])}, {'tools': int(val.strip().split('=')[3])}]
+                #TODO: Fix this. Taking too long right now hence skipping for later.
+
+    except Exception as e:
+        print(f"Error occurred while reading job description: {e}")
+
+    requirement = {
+        'position': position,
+        'expected_experience': expected_experience,
+        'skills': skills,
+        'tools': tools,
+        # 'value': value TODO
+    }
+
+    # print(requirement)
+
+    return requirement
+
+
 resume_path = "./ignore/Aksh.pdf"
 print(ingest_profile(resume_path)) #0.13 secs per run on #test_resume0 (without print)
+job_description_path = "./ignore/job_description.txt"
+print(ingest_job_description(job_description_path)) #0.00005 secs per sun on #test_desc (without print)
